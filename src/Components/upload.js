09a183f2491dde3a53afd0e45861da0ad3c97c42
SarthakaJha <SarthakaJha@users.noreply.github.com>
2023-02-02 15:46:36 +0100

    .

--- src/Components/upload.js
+++ src/Components/upload.js
@@ -47,7 +47,8 @@ function handleFile(files) {
     };
     
     return (
-      <form clasName='upload' id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+      <div className='upload'>
+      <form  id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
         <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
         <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
           <div>
@@ -57,6 +58,7 @@ function handleFile(files) {
         </label>
         { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
       </form>
+      </div>
     );
   };
   export default DragDropFile;
